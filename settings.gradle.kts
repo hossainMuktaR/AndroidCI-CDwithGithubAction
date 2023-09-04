42a54b1556a39929f0b61d72119424635ab95176
Hossain MuktaR <HossainMuktarDev@gmail.com>
2023-09-05 01:31:34 +0600

    initial State

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CI-CD Pipeline With GitHub Action"
+include(":app")
